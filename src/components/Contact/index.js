7c14e07979f6499b62c79def8cc1c08bd8aeeedd
aksmith5239 <aklobby@gmail.com>
2020-12-19 15:35:14 -0700

    contact form valdiation completed

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -19,7 +19,7 @@ function ContactForm() {
             }
         } else {
             if(!e.target.value.length) {
-                setErrorMessage(`${e.target.name} is required.`)
+                setErrorMessage(`Please enter a ${e.target.name}.`)
             } else {
                 setErrorMessage('');
             }
@@ -39,19 +39,19 @@ function ContactForm() {
             <form >
                 <div>
                     <label htmlFor="name">Name: </label>
-                    <input type="text" defaultValue={name} onChange={handleChange} name="name"   />
+                    <input type="text" defaultValue={name} onBlur={handleChange} name="name"   />
                 </div>
                 <div>
                     <label htmlFor="email">Email: </label>
-                    <input type="email"  defaultValue={email} onChange={handleChange} name="email" />
+                    <input type="email"  defaultValue={email} onBlur={handleChange} name="email" />
                 </div>
                 <div>
                     <label htmlFor="message">Message: </label>
-                    <textarea name="message" rows="5" defaultValue={message} onChange={handleChange} />
+                    <textarea name="message" rows="5" defaultValue={message} onBlur={handleChange} />
                 </div>
                 {errorMessage && (
                     <div>
-                        <p className="error-message">Error Message</p>
+                        <p className="error-message">{errorMessage}</p>
                     </div>
                     )}
                 <button type="submit">Submit</button>
